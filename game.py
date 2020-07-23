63acb9d88a043b6887aeb4cf3eb68a3ebd1bfc73
cosikowy <mr.cosik@gmail.com>
2020-07-23 10:02:47 +0200

    attack detecion after move for stones

--- game.py
+++ game.py
@@ -67,15 +67,15 @@ class Board:
 
     def draw_board(self):
         print(
-            '  ', ' A', ' B', ' C', ' D', ' E' , ' F', ' G', ' H', '\n',
-            '8', self.board[0][0], self.board[0][1], self.board[0][2], self.board[0][3], self.board[0][4], self.board[0][5], self.board[0][6], self.board[0][7], '\n',
-            '7', self.board[1][0], self.board[1][1], self.board[1][2], self.board[1][3], self.board[1][4], self.board[1][5], self.board[1][6], self.board[1][7], '\n',
-            '6', self.board[2][0], self.board[2][1], self.board[2][2], self.board[2][3], self.board[2][4], self.board[2][5], self.board[2][6], self.board[2][7], '\n',
-            '5', self.board[3][0], self.board[3][1], self.board[3][2], self.board[3][3], self.board[3][4], self.board[3][5], self.board[3][6], self.board[3][7], '\n',
-            '4', self.board[4][0], self.board[4][1], self.board[4][2], self.board[4][3], self.board[4][4], self.board[4][5], self.board[4][6], self.board[4][7], '\n',
-            '3', self.board[5][0], self.board[5][1], self.board[5][2], self.board[5][3], self.board[5][4], self.board[5][5], self.board[5][6], self.board[5][7], '\n',
-            '2', self.board[6][0], self.board[6][1], self.board[6][2], self.board[6][3], self.board[6][4], self.board[6][5], self.board[6][6], self.board[6][7], '\n',
-            '1', self.board[7][0], self.board[7][1], self.board[7][2], self.board[7][3], self.board[7][4], self.board[7][5], self.board[7][6], self.board[7][7], '\n',
+            '   ', 'A0', 'B1', 'C2', 'D3', 'E4' , 'F5', 'G6', 'H7', '\n',
+            '08', self.board[0][0], self.board[0][1], self.board[0][2], self.board[0][3], self.board[0][4], self.board[0][5], self.board[0][6], self.board[0][7], '\n',
+            '17', self.board[1][0], self.board[1][1], self.board[1][2], self.board[1][3], self.board[1][4], self.board[1][5], self.board[1][6], self.board[1][7], '\n',
+            '26', self.board[2][0], self.board[2][1], self.board[2][2], self.board[2][3], self.board[2][4], self.board[2][5], self.board[2][6], self.board[2][7], '\n',
+            '35', self.board[3][0], self.board[3][1], self.board[3][2], self.board[3][3], self.board[3][4], self.board[3][5], self.board[3][6], self.board[3][7], '\n',
+            '44', self.board[4][0], self.board[4][1], self.board[4][2], self.board[4][3], self.board[4][4], self.board[4][5], self.board[4][6], self.board[4][7], '\n',
+            '53', self.board[5][0], self.board[5][1], self.board[5][2], self.board[5][3], self.board[5][4], self.board[5][5], self.board[5][6], self.board[5][7], '\n',
+            '62', self.board[6][0], self.board[6][1], self.board[6][2], self.board[6][3], self.board[6][4], self.board[6][5], self.board[6][6], self.board[6][7], '\n',
+            '71', self.board[7][0], self.board[7][1], self.board[7][2], self.board[7][3], self.board[7][4], self.board[7][5], self.board[7][6], self.board[7][7], '\n',
         )
 
 
@@ -105,11 +105,18 @@ class Stone:
         if self.color == 'white' and self.mode == 'queen':
             self.name = 'WQ'
 
+    def upgrade(self):
+        self.mode = 'queen'
+        if self.color == 'white':
+            self.name = 'WQ'
+        else:
+            self.name = 'BQ'
+
     def make_move(self, new_pos, game):
         attacked = False
         if game.turn == self.color:
-
             validation, event_place, new_pos, event = self.validate_move(self.pos, new_pos, self.mode, game, self.color)
+
             if validation:
                 if event:
                     print('Event: ', event)
@@ -117,11 +124,8 @@ class Stone:
                         game.board[event_place[1]][event_place[0]] = BlankSpace()
                         attacked = True
 
-
-                print('color: ', self.color)
                 print('ok validated')
 
-            
                 game.board[self.row][self.col] = BlankSpace()
                 game.board[new_pos[1]][new_pos[0]] = self
                 self.update_pos(new_pos)
@@ -152,7 +156,7 @@ class Stone:
             if isinstance(game.board[old_pos[1]-1][old_pos[0]+1],BlankSpace): # up/right
                 enviroment[(old_pos[0]+1,old_pos[1]-1)] = ((old_pos[0]+1,old_pos[1]-1), None)
             else:
-                if game.board[old_pos[1]-1][old_pos[0]+1]!=color and isinstance(game.board[old_pos[1]-1][old_pos[0]+1], BlankSpace):
+                if game.board[old_pos[1]-1][old_pos[0]+1]!=color and isinstance(game.board[old_pos[1]-2][old_pos[0]+2], BlankSpace):
                     enviroment[(old_pos[0]+1,old_pos[1]-1)] = ((old_pos[0]+2,old_pos[1]-2), 'delete')
         except IndexError:
             pass
@@ -226,15 +230,6 @@ class Stone:
         else:
             pass
 
-        # if isinstance(board[new_pos[1]][new_pos[0]],BlankSpace):
-        #     pass
-        # else:
-        #     print('position is occupied')
-        #     return False
-        
-        #     return True
-        # else:
-        #     return False
 
     def __repr__(self):
         return self.name
@@ -252,11 +247,12 @@ while True:
     old_pos = input('Pick stone: ')
     new_pos = input("What's ur move? ")
     old_pos, new_pos = b.decode_move(old_pos, new_pos)
+
     selected = b.board[old_pos[1]][old_pos[0]]
 
     if isinstance(selected, Stone):
         b, attacked, new_pos = b.board[old_pos[1]][old_pos[0]].make_move(new_pos, b)
-        print(attacked)
+        print('new_pos_2: ',new_pos)
         if attacked:
             possible_attack = False
             enviroment = selected.stone_enviroment(new_pos, b, b.turn)
@@ -265,6 +261,7 @@ while True:
                 if value[1] == 'delete':
                     possible_attack = True
                     break
+            print('Possible attack: ', possible_attack)
             if possible_attack:
                 possible_attack = False
                 if b.turn == 'white':
