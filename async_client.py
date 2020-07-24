bbcba1dbc416cfe795164f0b2ba936425d4bd3b6
cosikowy <mr.cosik@gmail.com>
2020-07-24 14:51:50 +0200

    basic connection client<->server<->client2

---
+++ async_client.py
@@ -0,0 +1,40 @@
+import asyncio
+import socket
+import json
+
+async def wait_for_data():
+    loop = asyncio.get_running_loop()
+
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost',5000))
+
+    reader, writer = await asyncio.open_connection(sock=client)
+    
+    connected = True
+    name = input('name: ')
+    while connected:
+        msg = input('msg: ')
+
+        send_dict = {'name':name, 'msg':msg}
+        message = json.dumps(send_dict)
+        loop.call_soon(client.send, message.encode())
+
+        data = await reader.read(2048)
+
+        if send_dict['msg'] == 'close':
+            writer.close()
+            client.close()
+            connected = False
+        else:
+            data = await reader.read(2048)
+
+        print("Received:", data.decode())
+        if msg == 'close':
+            writer.close()
+            client.close()
+            connected = False
+
+
+
+asyncio.run(wait_for_data())
---
+++ async_server.py
@@ -0,0 +1,57 @@
+import asyncio
+import json
+import ast
+
+
+async def handle_connection(reader, writer):
+    global players
+    
+    while True:
+
+        data = await reader.read(2048)
+        message = data.decode()
+        msg_json = json.loads(message)
+        addr = writer.get_extra_info('peername')
+
+        print(f"Received {msg_json['name']} from {addr}")
+        print(f"Received {msg_json['msg']} from {addr}")
+
+        players[msg_json['name']] = writer
+
+
+        print(f"Send: {message!r}")
+        for player in players.keys():
+            if player!=msg_json['name']:
+                writer2 = players[player]
+                writer2.write(data)
+                await writer2.drain()
+            if msg_json['msg']=='close':
+                writer2 = players[player]
+                writer2.write(data)
+                await writer2.drain()
+        
+        writer.write(data)
+        await writer.drain()
+        
+        
+        if message == '':
+            print("Connection lost")
+            writer.close()
+
+        if msg_json['msg'] == 'close':
+            print("Close the connection")
+            writer.close()
+
+
+players = {}
+async def main():
+    server = await asyncio.start_server(
+        handle_connection, '127.0.0.1', 5000)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
