cc49039e1302096a8d564eefdb56ed951369d620
cosikowy <mr.cosik@gmail.com>
2020-07-25 17:33:48 +0200

    communication done

--- async_client.py
+++ async_client.py
@@ -5,7 +5,13 @@ import pickle
 
 async def wait_for_data():
     loop = asyncio.get_running_loop()
-
+    send_dict = {
+            'name':'',
+            'msg':'',
+            'game_id': '',
+            'player_status':'new'
+            }
+        
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('localhost',5000))
@@ -13,40 +19,86 @@ async def wait_for_data():
     reader, writer = await asyncio.open_connection(sock=client)
     
     connected = True
-    name = input('name: ')
-    # game_state = 'Game State v1'
+    send_dict['name'] = input('name: ')
+    incorrect_name = True
+    while incorrect_name:
+        name_msg = json.dumps(send_dict)
+        loop.call_soon(client.send, name_msg.encode())
+        response = await reader.read(2048)
+        response = response.decode()
+        print(response)
+        if response != 'Name already used!':
+            incorrect_name = False
+            send_dict['player_status'] = 'existing'
+        else:
+            send_dict['name'] = input('name: ')
+
+    no_enemy = True
+    
+    try:
+        response = json.loads(response)
+        if response['game_id']!='':
+            no_enemy = False
+    except:
+        pass
+
+    while no_enemy:
+        response = await reader.read(2048)
+        response = response.decode()
+        print('waiting for enemy')
+        if response != 'wait':
+            no_enemy = False
+    try:
+        response = json.loads(response)
+    except:
+        pass
+    send_dict['game_id'] = response['game_id']
+    send_dict['color'] = response['color']
+    send_dict['player_status'] = 'existing'
+
     while connected:
+        
+        # print(send_dict)
         print('board')
-        print('Your move')
-        msg = input('msg: ')
-        print('board_updated')
-        send_dict = {
-            'name':name,
-            'msg':msg
-            }
-        message = json.dumps(send_dict)
-        loop.call_soon(client.send, message.encode())
+        
+        if send_dict['color']=='white':
+            print('Your move')
+            send_dict['move'] = input('msg: ')
+            print('board_updated')
 
-        print("Waiting for oponent's move")
-        data = await reader.read(2048)
-        # print(game_state)
+            message = json.dumps(send_dict)
+            loop.call_soon(client.send, message.encode())
 
-        # print("Oponent's move:", data.decode())
-        # loop.call_soon(client.send, 'sync'.encode())
+            print("Waiting for oponent's move")
+            data = await reader.read(2048)
+            data = json.loads(data.decode())
+            print('Enemy move:',data['move'])
+        else:
+            print("Waiting for oponent's move")
+            data = await reader.read(2048)
+            data = json.loads(data.decode())
+            print('Enemy move:',data['move'])
+            print('board_updated')
+            print('Your move')
+            send_dict['move'] = input('msg: ')
+            print('board_updated')
+            message = json.dumps(send_dict)
+            loop.call_soon(client.send, message.encode())
+
+        
+        
+        
+        
+        
+        
         if send_dict['msg'] == 'close':
             writer.close()
             client.close()
             connected = False
-        else:
-            sync = await reader.read(2048)
-        
-        print('board_synced, data recived 1st turn:', data)
-        print("Oponent's move:", sync.decode())
 
-        # loop.call_soon(client.send, 'awaiting'.encode())
-        # awaiting = await reader.read(2048)
-        
-        if msg == 'close':
+
+
+        if send_dict['msg'] == 'close':
             writer.close()
             client.close()
             connected = False
--- async_server.py
+++ async_server.py
@@ -2,61 +2,209 @@ import asyncio
 import json
 import ast
 import pickle
+import random
 
 
-async def handle_connection(reader, writer):
-    global players
+
+# async def handle_connection(reader, writer):
+#     global players
     
-    while True:
+#     while True:
 
-        data = await reader.read(2048)
-        # data2 = data
-        message = data.decode()
+#         data = await reader.read(2048)
+#         # data2 = data
+#         message = data.decode()
 
-        msg_json = json.loads(message)
-        addr = writer.get_extra_info('peername')
+#         msg_json = json.loads(message)
+#         players[msg_json['name']] = writer
+#         addr = writer.get_extra_info('peername')
 
-        print(f"Received {msg_json['name']} from {addr}")
-        print(f"Received {msg_json['msg']} from {addr}")
+#         print(f"Received {msg_json['msg']} from {msg_json['name']}   - {addr}")
+#         print(f"Send: {message}")
 
-        players[msg_json['name']] = writer
-
-
-        print(f"Send: {message!r}")
-        for player in players.keys():
-            if player!=msg_json['name']:
-                writer2 = players[player]
-                writer2.write(data)                 # Ta część jest jak będzie już 2 graczy w lobby
-                await writer2.drain()
-            if msg_json['msg']=='close':
-                writer2 = players[player]
-                writer2.write(data)
-                await writer2.drain()
         
-        # sync = await reader.read(2048)
-        # sync = sync.decode()
-        writer.write(data)
+
+#         for player in players.keys():
+#             if player!=msg_json['name']:
+#                 writer2 = players[player]
+#                 writer2.write(data)                 # Ta część jest jak będzie już 2 graczy w lobby
+#                 await writer2.drain()
+#             if msg_json['msg']=='close':
+#                 writer2 = players[player]
+#                 writer2.write(data)
+#                 await writer2.drain()
+
+#         writer.write(data)
+#         await writer.drain()
+
+
+#         if message == '':
+#             print("Connection lost")
+#             writer.close()
+
+#         if msg_json['msg'] == 'close':
+#             print("Close the connection")
+#             writer.close()
+
+
+# players = {}
+# async def main():
+#     server = await asyncio.start_server(
+#         handle_connection, '127.0.0.1', 5000)
+
+#     addr = server.sockets[0].getsockname()
+#     print(f'Serving on {addr}')
+
+#     async with server:
+#         await server.serve_forever()
+
+# asyncio.run(main())
+
+
+class Server:
+    players = {}
+    queue = []
+    lobby = {}
+    def __init__(self):
+        asyncio.run(self.main())
+    
+    async def main(self):
+        self.server = await asyncio.start_server(
+            self.handle_connection, '127.0.0.1', 5000,
+            reuse_address=True)
+
+        addr = self.server.sockets[0].getsockname()
+        print(f'Serving on {addr}')
+
+        async with self.server:
+            await self.server.serve_forever()
+
+    async def response_to_player(self, name, response):
+        writer = self.players[name]
+        print('response to:',name)
+        if isinstance(response, dict):
+            return_msg = json.dumps(response).encode()
+        else:
+            try:    
+                return_msg = response.encode()
+            except:
+                return_msg = response
+        writer.write(return_msg)
         await writer.drain()
 
+    async def read_data(self, reader_obj):
+        data = await reader.read(2048)
+        message = data.decode()
+        try:
+            msg_json = json.loads(message)
+        except:
+            msg_json = message
+        return msg_json
 
-        if message == '':
-            print("Connection lost")
-            writer.close()
+    def check_names(self, name):
+        if name in players.keys():
+            return True 
+        else:
+            return False
 
-        if msg_json['msg'] == 'close':
-            print("Close the connection")
-            writer.close()
+    async def handle_connection(self, reader, writer):
+        self.running_games = set()
+        response = {
+            'move':'',
+            'game_id':'',
+            'game_status':'',
+            'color':'white'
+        }
+        
+        while True:
+            name_exist = True
+            data = await reader.read(2048)
+            message = data.decode()
+            msg_json = json.loads(message)
+            
+            
+            print('ping')
+            
+            if msg_json['player_status']!='existing':
+                if msg_json['player_status'] == 'new' and msg_json['name'] not in self.players.keys():
+                    self.players[msg_json['name']] = writer
+                elif msg_json['player_status'] == 'new' and msg_json['name'] in self.players.keys():
+                    print('naming went wrong')
+                    return_msg = 'Name already used!'.encode()
+                    writer.write(return_msg)
+                    await writer.drain()
+                    data = await reader.read(2048)
+                    message = data.decode()
+                    msg_json = json.loads(message)
+                elif msg_json['player_status'] == 'exist':
+                    pass
+            if msg_json['name'] not in self.players.keys():
+                self.players[msg_json['name']] = writer
+            
+            
+            if msg_json['game_id']=='':
+                print('game_creation')
+                self.queue.append(msg_json['name'])
+                if len(self.queue)==2:
+                    game_id = random.randint(1000,9999)
+                    not_valid = True
+                    while not_valid:
+                        if game_id in self.running_games:
+                            game_id = random.randint(1000,9999)
+                        else:
+                            not_valid = False
+                    response['game_id'] = game_id
+                    self.lobby[game_id] = [x for x in self.queue]
+                    self.queue.clear()
+                    print(self.lobby)
+                    
+                    player_one = self.lobby[game_id][0]
+                    player_two = self.lobby[game_id][1]
+                    await self.response_to_player(player_one, response)
+                    response_p2 = response
+                    response_p2['color'] = 'black'
+                    await self.response_to_player(player_two, response_p2)
 
 
-players = {}
-async def main():
-    server = await asyncio.start_server(
-        handle_connection, '127.0.0.1', 5000)
+                    
+                else:
+                    return_msg = 'wait'.encode()
+                    writer.write(return_msg)
+                    await writer.drain()
+                print('game created')
 
-    addr = server.sockets[0].getsockname()
-    print(f'Serving on {addr}')
 
-    async with server:
-        await server.serve_forever()
+            elif msg_json['game_id']!='':
+                player_one = self.lobby[msg_json['game_id']][0]
+                player_two = self.lobby[msg_json['game_id']][1]
+                
+                print('ping2')
 
-asyncio.run(main())
+                addr = writer.get_extra_info('peername')
+                print(f"Received {msg_json} from {msg_json['msg']} - {addr}")
+                response['move'] = msg_json['move']
+                print(f"Send: {response}")
+                
+                response = json.dumps(response).encode()
+                
+                if msg_json['color']=='white':
+                    await self.response_to_player(player_two, response)
+                else:
+                    await self.response_to_player(player_one, response)
+
+                # writer.write(response)
+                # await writer.drain()
+                response = json.loads(response.decode())
+
+
+
+
+            if message == '':
+                print("Connection lost")
+                writer.close()
+
+            if msg_json['msg'] == 'close':
+                print("Close the connection")
+                writer.close()
+
+Server()
