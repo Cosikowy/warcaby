c57595a18dd00eedde4a84f248ff7bbdd451ce29
Cosikowy <alek.jagiello@gmail.com>
2020-12-07 12:41:46 +0100

    draw board refactor complete

--- game_ref.py
+++ game_ref.py
@@ -1,44 +1,64 @@
 
 
 
-
 class Board:
-    players = []
-    
-    def __init__(self, rows, cols, p1 = 'p1', p2 = 'p2'):
-        self.board = [[Placeholder() for x in range(cols)] for _ in range(rows)]
+    def __init__(self, p1 = 'p1', p2 = 'p2'):
+        self.board = [[Placeholder() for x in range(8)] for _ in range(8)]
         self.player_1 = p1
         self.player_2 = p2
+        self.players = (self.player_1, self.player_2)
+        
+        self.white_starting_pos = (
+            (0,2), (2,2), (4,2), (6,2),
+            (1,1), (3,1), (5,1), (7,1),
+            (0,0), (2,0), (4,0), (6,0)
+        )
+        
+        self.black_starting_pos = (
+            (1,7), (3,7), (5,7), (7,7),
+            (0,6), (2,6), (4,6), (6,6),
+            (1,5), (3,5), (5,5), (7,5)
+        )
+        for white, black in zip(self.white_starting_pos, self.black_starting_pos):
+            self.board[white[0]][white[1]] = Stone()
+            self.board[black[0]][black[1]] = Stone('black')
         
     def draw_board(self):
-        print(
-            '  ',' |', ' A',' |', ' B',' |', ' C',' |', ' D',' |', ' E' ,' |', ' F',' |', ' G',' |', ' H', '\n', \
-            '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '1', ' |', self.board[0][7],' |',  self.board[1][7],' |', self.board[2][7],' |', self.board[3][7],' |', self.board[4][7],' |', self.board[5][7],' |', self.board[6][7],' |', self.board[7][7], '\n', \
-            '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '2', ' |', self.board[0][6],' |',  self.board[1][6],' |', self.board[2][6],' |', self.board[3][6],' |', self.board[4][6],' |', self.board[5][6],' |', self.board[6][6],' |', self.board[7][6], '\n', \
-            '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '3', ' |', self.board[0][5],' |',  self.board[1][5],' |', self.board[2][5],' |', self.board[3][5],' |', self.board[4][5],' |', self.board[5][5],' |', self.board[6][5],' |', self.board[7][5], '\n', \
-            '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '4', ' |', self.board[0][4],' |',  self.board[1][4],' |', self.board[2][4],' |', self.board[3][4],' |', self.board[4][4],' |', self.board[5][4],' |', self.board[6][4],' |', self.board[7][4], '\n', \
-            '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '5', ' |', self.board[0][3],' |',  self.board[1][3],' |', self.board[2][3],' |', self.board[3][3],' |', self.board[4][3],' |', self.board[5][3],' |', self.board[6][3],' |', self.board[7][3], '\n', \
-            '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '6', ' |', self.board[0][2],' |',  self.board[1][2],' |', self.board[2][2],' |', self.board[3][2],' |', self.board[4][2],' |', self.board[5][2],' |', self.board[6][2],' |', self.board[7][2], '\n', \
-            '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '7', ' |', self.board[0][1],' |',  self.board[1][1],' |', self.board[2][1],' |', self.board[3][1],' |', self.board[4][1],' |', self.board[5][1],' |', self.board[6][1],' |', self.board[7][1], '\n', \
-            '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '8', ' |', self.board[0][0],' |', self.board[1][0],' |', self.board[2][0], ' |', self.board[3][0],' |', self.board[4][0],' |', self.board[5][0],' |', self.board[6][0],' |', self.board[7][0], '\n', \
-        )
-
-    def make_move(self):
-        pass
+        columns_row = ('  ',' |', ' A',' |', ' B',' |', ' C',' |', ' D',' |', ' E' ,' |', ' F',' |', ' G',' |', ' H', '\n')
+        spacer = ('-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n')
+        board = [*columns_row, *spacer]
+        def print_row(number):
+            row = []
+            for x in range(8):
+                row.append(' |')
+                row.append(self.board[x][number])
+            row.append('\n')
+            return (f'{number+1}', *row)
+        for row in reversed(range(8)):
+            board.extend(print_row(row))
+            board.extend(spacer)
+        print(*board)
     
-    def check_for_promote(self):
+    def make_move(self, old_pos, new_pos):
+        if isinstance(self.board[old_pos[0]][old_pos[1]], Stone):
+            if isinstance(self.board[new_pos[0]][new_pos[1]]):
+                self.board[new_pos[0]][new_pos[1]], self.board[old_pos[0]][old_pos[1]] = \
+                self.board[old_pos[0]][old_pos[1]], Placeholder()
+                return True
+            else:
+                raise TypeError("It's ain't no free place")
+        else:
+            raise TypeError("It's ain't no stone!")
+    
+    def check_for_promote(self, pos):
+        white_promote = ()
+        black_promote = ()
         pass
     
     def check_winner(self):
         pass
+
+
 
 
 
@@ -51,18 +71,15 @@ class Stone:
     def __repr__(self):
         return f'{self.color[0]}{self.type[0]}'
 
-
+ 
 class Placeholder:
-    def __init__(self):
-        self.name = 'BlankSpace'
-
     def __repr__(self):
         return ' x'
 
 
 # class Game:...
-b = Board(8,8)
-# b.draw_board()
+b = Board()
+b.draw_board()
 
-s = Stone('b', 'q')
-print(s)
+# s = Stone('b', 'q')
+# print(s)
