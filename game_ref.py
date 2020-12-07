1fff63efe50135ef710f6e431fd82c549aa47ade
Cosikowy <alek.jagiello@gmail.com>
2020-12-07 09:19:33 +0100

    board flipped

--- game_ref.py
+++ game_ref.py
@@ -11,35 +11,45 @@ class Board:
         self.player_2 = p2
         
     def draw_board(self):
-        
         print(
             '  ',' |', ' A',' |', ' B',' |', ' C',' |', ' D',' |', ' E' ,' |', ' F',' |', ' G',' |', ' H', '\n', \
             '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '8', ' |', self.board[0][0],' |', self.board[0][1],' |', self.board[0][2], ' |', self.board[0][3],' |', self.board[0][4],' |', self.board[0][5],' |', self.board[0][6],' |', self.board[0][7], '\n', \
+            '1', ' |', self.board[0][7],' |',  self.board[1][7],' |', self.board[2][7],' |', self.board[3][7],' |', self.board[4][7],' |', self.board[5][7],' |', self.board[6][7],' |', self.board[7][7], '\n', \
             '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '7', ' |', self.board[1][0],' |',  self.board[1][1],' |', self.board[1][2],' |', self.board[1][3],' |', self.board[1][4],' |', self.board[1][5],' |', self.board[1][6],' |', self.board[1][7], '\n', \
+            '2', ' |', self.board[0][6],' |',  self.board[1][6],' |', self.board[2][6],' |', self.board[3][6],' |', self.board[4][6],' |', self.board[5][6],' |', self.board[6][6],' |', self.board[7][6], '\n', \
             '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '6', ' |', self.board[2][0],' |',  self.board[2][1],' |', self.board[2][2],' |', self.board[2][3],' |', self.board[2][4],' |', self.board[2][5],' |', self.board[2][6],' |', self.board[2][7], '\n', \
+            '3', ' |', self.board[0][5],' |',  self.board[1][5],' |', self.board[2][5],' |', self.board[3][5],' |', self.board[4][5],' |', self.board[5][5],' |', self.board[6][5],' |', self.board[7][5], '\n', \
             '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '5', ' |', self.board[3][0],' |',  self.board[3][1],' |', self.board[3][2],' |', self.board[3][3],' |', self.board[3][4],' |', self.board[3][5],' |', self.board[3][6],' |', self.board[3][7], '\n', \
+            '4', ' |', self.board[0][4],' |',  self.board[1][4],' |', self.board[2][4],' |', self.board[3][4],' |', self.board[4][4],' |', self.board[5][4],' |', self.board[6][4],' |', self.board[7][4], '\n', \
             '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '4', ' |', self.board[4][0],' |',  self.board[4][1],' |', self.board[4][2],' |', self.board[4][3],' |', self.board[4][4],' |', self.board[4][5],' |', self.board[4][6],' |', self.board[4][7], '\n', \
+            '5', ' |', self.board[0][3],' |',  self.board[1][3],' |', self.board[2][3],' |', self.board[3][3],' |', self.board[4][3],' |', self.board[5][3],' |', self.board[6][3],' |', self.board[7][3], '\n', \
             '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '3', ' |', self.board[5][0],' |',  self.board[5][1],' |', self.board[5][2],' |', self.board[5][3],' |', self.board[5][4],' |', self.board[5][5],' |', self.board[5][6],' |', self.board[5][7], '\n', \
+            '6', ' |', self.board[0][2],' |',  self.board[1][2],' |', self.board[2][2],' |', self.board[3][2],' |', self.board[4][2],' |', self.board[5][2],' |', self.board[6][2],' |', self.board[7][2], '\n', \
             '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '2', ' |', self.board[6][0],' |',  self.board[6][1],' |', self.board[6][2],' |', self.board[6][3],' |', self.board[6][4],' |', self.board[6][5],' |', self.board[6][6],' |', self.board[6][7], '\n', \
+            '7', ' |', self.board[0][1],' |',  self.board[1][1],' |', self.board[2][1],' |', self.board[3][1],' |', self.board[4][1],' |', self.board[5][1],' |', self.board[6][1],' |', self.board[7][1], '\n', \
             '-',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +',' -',' +', ' -', '\n', \
-            '1', ' |', self.board[7][0],' |',  self.board[7][1],' |', self.board[7][2],' |', self.board[7][3],' |', self.board[7][4],' |', self.board[7][5],' |', self.board[7][6],' |', self.board[7][7], '\n', \
+            '8', ' |', self.board[0][0],' |', self.board[1][0],' |', self.board[2][0], ' |', self.board[3][0],' |', self.board[4][0],' |', self.board[5][0],' |', self.board[6][0],' |', self.board[7][0], '\n', \
         )
+
+    def make_move(self):
+        pass
+    
+    def check_for_promote(self):
+        pass
+    
+    def check_winner(self):
+        pass
+
+
 
 class Stone:
     def __init__(self, color = None, stone_type = None):
         self.name = "Stone"
-        self.color = color if color else 'white'
-        self.type = stone_type if stone_type else 'stone'
+        self.color = color.capitalize() if color else 'White'
+        self.type = stone_type.capitalize() if stone_type else 'Stone'
     
     def __repr__(self):
-        return f''
+        return f'{self.color[0]}{self.type[0]}'
 
 
 class Placeholder:
@@ -52,4 +62,7 @@ class Placeholder:
 
 # class Game:...
 b = Board(8,8)
-b.draw_board()
+# b.draw_board()
+
+s = Stone('b', 'q')
+print(s)
